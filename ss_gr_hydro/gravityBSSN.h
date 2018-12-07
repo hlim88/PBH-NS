dceda7d19a1dc767530ecb40dff1734ef4e9109e
Hyun Lim <hylim1988@gmail.com>
2018-12-07 14:17:08 -0500

    Using SS-BSSN for EE. Choosing different gauge choice than previous

---
+++ ss_gr_hydro/gravityBSSN.h
@@ -0,0 +1,18 @@
+#define NUM_NEWT_ITER (400) //Maximum number of allowed Newton iterations
+#define REL_ERROR (1.0e-9)  //Relative error tolerance in solution
+
+/*
+   Define BSSN eqns with usual 1+log slicing and gamma driver
+*/
+
+// For alpha
+
+// For trK
+
+// For a
+
+// For b
+
+// For A
+
+// For constraint
