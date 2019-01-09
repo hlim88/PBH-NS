862172b9e912b38e1c35ce4941e452496f62e2cf
Hyun Lim <hylim1988@gmail.com>
2019-01-09 11:29:20 -0500

    Constraint monitoring. Need to check

--- ss_gr_hydro/analysis.c
+++ ss_gr_hydro/analysis.c
@@ -12,7 +12,8 @@ void unpertTOV_comp(){
 
 }
 
-void constraintcheck(){
+double computeConstraintL2(const double *constraintVec, const double *maskVec, 
+                    unsigned int lbegin, unsigned int lend){
 
   for (unsigned int k = 3; k < nz - 3; k++) {
     double z = pmin[2] + k*hz;
@@ -21,6 +22,50 @@ void constraintcheck(){
       for (unsigned int i = 3; i < nx - 3; i++) {
         double x = pmin[0] + i*hx;
         unsigned int pp = i + nx * (j + ny * k);
+
+        double l2=0.0;
+        double l2_g=0.0;
+        const double MASK_THRESHOLD=1.0;
+        for(unsigned int i=lbegin;i<lend;i++)
+        {
+            if(maskVec[i]<MASK_THRESHOLD)
+                l2+=(constraintVec[i]*constraintVec[i]*maskVec[i]*maskVec[i]
+                   *maskVec[i]*maskVec[i]);
+            else
+                l2+=(constraintVec[i]*constraintVec[i]);
+        }
+
+        return (sqrt(l2_g));
+
+}
+
+double printL2Constraint(const double **constraintVec, unsigned int timeSteps){
+
+  const unsigned int numConstVars; //4?
+
+  double constraintL2[numConstVars];
+
+  for (unsigned int id = 0; index < numConstVars; id++) {
+      constraintL2[id] = computeConstraintL2();
+      printf("Constraint : %lf\n",constraintL2[id]);
+  }
+
+  //TODO : Add routine to generate output using sprintf?
+  #if 0
+    ofstream fileConst;
+    char fname[256];
+    sprintf(fname,"%s_constraint.dat",val_name_
+    if(timeSteps == 0)
+    fileConst.open(fname)
+    printf("Time steps : %d",timeSteps);
+    printf("HAM : %lf",constraintL2[0]);
+    printf("MOM1 : %lf",constraintL2[1]; //just 1d?
+    fileConst.close();
+
+    return 0;
+
+  #endif
+
 
 }
 
